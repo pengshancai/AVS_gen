eab86d025ceb73a95f20085f13581997634ea43a
pengshan <pengshan@pengshans-MacBook-Pro.local>
2022-09-07 20:10:01 -0400

    update

--- run_summarization.py
+++ run_summarization.py
@@ -336,7 +336,7 @@ def main():
         if data_args.test_file is not None:
             data_files["test"] = data_args.test_file
             extension = data_args.test_file.split(".")[-1]
-        datasets = load_dataset(extension, data_files=data_files, field='data')
+        datasets = load_dataset(extension, data_files=data_files)
     # See more about loading any type of standard or custom dataset (from files, python dict, pandas DataFrame, etc) at
     # https://huggingface.co/docs/datasets/loading_datasets.html.
 
